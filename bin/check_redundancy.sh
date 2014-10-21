ea8c8ea6fb7346d1e481c4a62c42f0bb9c343900
Daniel Nilsson <daniel.nilsson@scilifelab.se>
2014-10-21 11:46:50 +0200

    Added redundancy check and hgnc gene symbol lookup scripts.

---
+++ bin/check_redundancy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+status=0
+
+for file in *txt;
+do 
+    redundant_line_count=`cat $file |sort |uniq -c | grep -cv \ 1 `
+    if [ $redundant_line_count -gt 1 ] 
+    then
+	echo Redundant lines found in $file!
+	cat $file |sort |uniq -c | grep -v \ 1 
+	status=1
+    fi
+done
+
+return $status
---
+++ bin/hgnc_gene_symbol_verify.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+status=0
+
+for file in *txt;
+do
+    for genesymbol in `cat $file` ; 
+    do 
+	genesymbolcount=`wget -qO- --header='Accept:text/xml' http://rest.genenames.org/search/symbol/$genesymbol |grep -c $genesymbol`
+	if [ $genesymbolcount -ne 1 ] 
+	then
+	    echo "Failed lookup for HGNC $genesymbol in $file.";
+	    status=1
+	fi
+    done
+done
